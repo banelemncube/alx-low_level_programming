9ed6f9b0107ece680566612da8f75b337c519690
Banele Mncube <mncubebanele@gmail.com>
2022-09-26 08:46:54 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,15 @@
-/*                                                                                                                                   
- * File: 0-memset.c                                                                                                                  
- * Auth: not                                                                                                                         
- */                                                                                                                                  
-                                                                                                                                     
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
- * _memset - Fills the first n bytes of the memory area                                                                              
- *          pointed to by @s with the constant byte @c.                                                                              
- * @s: A pointer to the memory area to be filled.                                                                                    
- * @c: The character to fill the memory area with.                                                                                   
- * @n: The number of bytes to be filled.                                                                                             
- * description _memset: over there                                                                                                   
- *                                                                                                                                   
- * Return: A pointer to the filled memory area @s.                                                                                   
- *                                                                                                                                   
- */                                                                                                                                  
-void *_memset(void *s, int c, size_t n)
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: memory area
+ * @b: constant byte b
+ * @n: first n bytes of memory area pointed by s
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
