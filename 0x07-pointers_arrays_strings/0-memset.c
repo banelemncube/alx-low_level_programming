0a540a463114458799a2d7d4307148dbb3a6c6a4
Banele Mncube <mncubebanele@gmail.com>
2022-09-26 10:53:56 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -16,8 +16,8 @@ void *_memset(void *s, int c, size_t n)
      unsigned int index;
      unsigned char *memory = s, value = c;
 
-    for (index = 0; index <n; index++)
+    for (index = 0; index < n; index++)
           memory[index] = value;
 
-    return(memry);
+    return (memory);
 }
