a9b29b0e740a04ca35b2bbe16fa85f98a8e0d8ad
Banele Mncube <mncubebanele@gmail.com>
2022-09-26 15:04:44 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,7 +8,9 @@
 * Return: If c is found - a pointer to the first occurence.
 *If c is not found - NULL.
 */
+
 char *_strchr(char *s, char c)
+
 {
 int index;
 for (index = 0; s[index] >= '\0'; index++)
